239331e9227d876fc173ac7a2b280341d1ca5d61
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-03 15:04:24 +0100

    first commit

---
+++ routes/pdf.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const PDFDocument = require('pdfkit')
+
+router.post('/', (req, res) => {
+  const doc = new PDFDocument()
+  let filename = req.body.filename
+  // Stripping special characters
+  filename = encodeURIComponent(filename) + '.pdf'
+  // Setting response to attachment (download).
+  // If you use 'inline' here it will automatically open the PDF
+  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"')
+  res.setHeader('Content-type', 'application/pdf')
+  const content = req.body.content
+  doc.y = 300
+  doc.text(content, 50, 50)
+  doc.pipe(res)
+  doc.end()
+})
+
+module.exports = router

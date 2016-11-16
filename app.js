00c0a3f3c721df59eb7b1096cde4e78813364191
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-16 17:45:17 +0100

    removes favicon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var path = require('path')
-var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
@@ -15,8 +14,6 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))

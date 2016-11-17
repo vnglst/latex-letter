ba609ea8c775f9417e95773366adfd476a5c8d60
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-17 16:32:27 +0100

    removes absolete scrips

--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update && sudo apt-get upgrade
-echo "Install docker"
-sudo curl -sSL https://get.docker.com/ | sh
-sudo usermod -aG docker ubuntu
-curl https://getcaddy.com | bash
-sudo reboot
--- scripts/startup.sh
+++
@@ -1,6 +0,0 @@
-ulimit -n 8192
-sudo nohup caddy >> logs/caddy.log &
-cat logs/caddy.log
-docker info
-docker pull vnglst/latex-letter
-docker run -p 1844:3000 -d "$_"
